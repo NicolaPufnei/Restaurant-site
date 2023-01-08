f412ce97a1b853c01a692003440131beff954e30
NicolaPufnei <nicolapufnei@gmail.com>
2023-01-08 15:52:20 +0200

    v8

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-const search = document.querySelector('.search')
-const menu = document.querySelector('.menu')
-const cart = document.querySelector('.cart')
-
-function searchButton() {
-    search.classList.replace('unselected' , 'selected')
-    menu.classList.replace('selected' , 'unselected')
-    cart.classList.replace('selected' , 'unselected')
-}
-function menuButton() {
-    search.classList.replace('selected' , 'unselected')
-    menu.classList.replace('unselected' , 'selected')
-    cart.classList.replace('selected' , 'unselected')
-}
-function cartButton() {
-    search.classList.replace('selected' , 'unselected')
-    menu.classList.replace('selected' , 'unselected')
-    cart.classList.replace('unselected' , 'selected')
-}
+document.getElementById("quatrostagioni").addEventListener("click", function() {
+    document.getElementById("quatro-stagioni-card").classList.toggle("is-active");
+  });

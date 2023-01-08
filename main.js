412d89270bc6236632dafd82402a45f8b8f669c3
NicolaPufnei <nicolapufnei@gmail.com>
2023-01-08 17:08:20 +0200

    v9

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 document.getElementById("quatrostagioni").addEventListener("click", function() {
     document.getElementById("quatro-stagioni-card").classList.toggle("is-active");
+    document.getElementById("shadow-background").classList.toggle("is-active");
+  });
+document.getElementById('shadow-background').addEventListener("click" , function() {
+  document.getElementById("quatro-stagioni-card").classList.toggle("is-active");
+  document.getElementById("shadow-background").classList.toggle("is-active");
   });

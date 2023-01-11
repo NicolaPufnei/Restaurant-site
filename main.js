53429b3cb54b08760d6619c3619e93408a43e561
NicolaPufnei <nicolapufnei@gmail.com>
2023-01-11 21:24:02 +0200

    v12

--- main.js
+++ main.js
@@ -2,14 +2,25 @@ document.getElementById("quatrostagioni").addEventListener("click", function() {
     document.getElementById("pizza-card-wrapper").classList.toggle("is-active");
     document.getElementById("quatro-stagioni-card").classList.toggle("is-active");
     document.getElementById("shadow-background").classList.toggle("is-active");
-  });
+});
+document.getElementById("mamamia").addEventListener("click", function() {
+    document.getElementById("pizza-card-wrapper").classList.toggle("is-active");
+    document.getElementById("quatro-stagioni-card").classList.toggle("is-active");
+    document.getElementById("shadow-background").classList.toggle("is-active");
+});
+document.getElementById("quatroformagi").addEventListener("click", function() {
+    document.getElementById("pizza-card-wrapper").classList.toggle("is-active");
+    document.getElementById("quatro-stagioni-card").classList.toggle("is-active");
+    document.getElementById("shadow-background").classList.toggle("is-active");
+});
+
 document.getElementById('shadow-background').addEventListener("click" , function() {
    document.getElementById("pizza-card-wrapper").classList.toggle("is-active");
    document.getElementById("quatro-stagioni-card").classList.toggle("is-active");
    document.getElementById("shadow-background").classList.toggle("is-active");
-   });
+});
 document.getElementById('close-button').addEventListener("click" , function() {
     document.getElementById("pizza-card-wrapper").classList.toggle("is-active");
     document.getElementById("quatro-stagioni-card").classList.toggle("is-active");
     document.getElementById("shadow-background").classList.toggle("is-active");
-    });
+});

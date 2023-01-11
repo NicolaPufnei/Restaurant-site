5999dfa0779809cc16cb920e12f785f8d3ba49c4
NicolaPufnei <nicolapufnei@gmail.com>
2023-01-11 20:48:25 +0200

    v11

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 document.getElementById("quatrostagioni").addEventListener("click", function() {
+    document.getElementById("pizza-card-wrapper").classList.toggle("is-active");
     document.getElementById("quatro-stagioni-card").classList.toggle("is-active");
     document.getElementById("shadow-background").classList.toggle("is-active");
   });
 document.getElementById('shadow-background').addEventListener("click" , function() {
+   document.getElementById("pizza-card-wrapper").classList.toggle("is-active");
    document.getElementById("quatro-stagioni-card").classList.toggle("is-active");
    document.getElementById("shadow-background").classList.toggle("is-active");
    });
+document.getElementById('close-button').addEventListener("click" , function() {
+    document.getElementById("pizza-card-wrapper").classList.toggle("is-active");
+    document.getElementById("quatro-stagioni-card").classList.toggle("is-active");
+    document.getElementById("shadow-background").classList.toggle("is-active");
+    });

f8425ea70d166755472683a2b8edfbef49a10352
NicolaPufnei <nicolapufnei@gmail.com>
2023-01-12 21:20:42 +0200

    v13

--- main.js
+++ main.js
@@ -1,26 +1,34 @@
 document.getElementById("quatrostagioni").addEventListener("click", function() {
-    document.getElementById("pizza-card-wrapper").classList.toggle("is-active");
-    document.getElementById("quatro-stagioni-card").classList.toggle("is-active");
-    document.getElementById("shadow-background").classList.toggle("is-active");
+    document.querySelector(".quatro-stagioni-card").classList.add("is-active");
+    document.querySelector(".quatro-stagioni").classList.add("is-active");
+    document.getElementById("shadow-background-1").classList.add("is-active");
 });
 document.getElementById("mamamia").addEventListener("click", function() {
-    document.getElementById("pizza-card-wrapper").classList.toggle("is-active");
-    document.getElementById("quatro-stagioni-card").classList.toggle("is-active");
-    document.getElementById("shadow-background").classList.toggle("is-active");
+    document.querySelector(".mamamia-card").classList.add("is-active");
+    document.querySelector(".pizza-mamamia").classList.add("is-active");
+    document.getElementById("shadow-background-2").classList.add("is-active");
 });
 document.getElementById("quatroformagi").addEventListener("click", function() {
-    document.getElementById("pizza-card-wrapper").classList.toggle("is-active");
-    document.getElementById("quatro-stagioni-card").classList.toggle("is-active");
-    document.getElementById("shadow-background").classList.toggle("is-active");
+    document.querySelector(".quatro-formagi-card").classList.add("is-active");
+    document.querySelector(".quatro-formagi").classList.add("is-active");
+    document.getElementById("shadow-background-3").classList.add("is-active");
 });
-
-document.getElementById('shadow-background').addEventListener("click" , function() {
-   document.getElementById("pizza-card-wrapper").classList.toggle("is-active");
-   document.getElementById("quatro-stagioni-card").classList.toggle("is-active");
-   document.getElementById("shadow-background").classList.toggle("is-active");
+document.getElementById('shadow-background-1').addEventListener("click" , function() {
+    document.querySelector(".quatro-stagioni-card").classList.remove("is-active");
+   document.getElementById("shadow-background-1").classList.remove("is-active");
+});
+document.getElementById('shadow-background-2').addEventListener("click" , function() {
+    document.querySelector(".mamamia-card").classList.remove("is-active");
+   document.getElementById("shadow-background-2").classList.remove("is-active");
+});
+document.getElementById('shadow-background-3').addEventListener("click" , function() {
+    document.querySelector(".quatro-formagi-card").classList.remove("is-active");
+   document.getElementById("shadow-background-3").classList.remove("is-active");
 });
 document.getElementById('close-button').addEventListener("click" , function() {
-    document.getElementById("pizza-card-wrapper").classList.toggle("is-active");
-    document.getElementById("quatro-stagioni-card").classList.toggle("is-active");
-    document.getElementById("shadow-background").classList.toggle("is-active");
+    document.getElementById("pizza-card-wrapper").classList.remove("is-active");
+    document.querySelector(".quatro-stagioni").classList.remove("is-active");
+    document.querySelector(".mamamia").classList.remove("is-active");
+    document.querySelector(".quatro-formagi").classList.remove("is-active");
+    document.getElementById("shadow-background").classList.remove("is-active");
 });
